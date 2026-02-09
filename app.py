f3f932977a746eb96c5fa3770f6852084aa0d549
Simone Asnake <134941433+erisimasol@users.noreply.github.com>
2026-02-09 19:48:23 +0300

    remove dep col

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ average_montly_hours = st.number_input("Average Monthly Hours", min_value=50, ma
 time_spend_company = st.number_input("Years at Company", min_value=1, max_value=20, value=3)
 work_accident = st.selectbox("Work Accident", [0,1])
 promotion_last_5years = st.selectbox("Promotion in Last 5 Years", [0,1])
-department = st.selectbox("Department", df['department'].unique())
 salary = st.selectbox("Salary Level", df['salary'].unique())
 
 # Prepare input row
@@ -48,7 +47,6 @@ input_dict = {
     "time_spend_company": time_spend_company,
     "Work_accident": work_accident,
     "promotion_last_5years": promotion_last_5years,
-    "department": department,
     "salary": salary
 }
 
